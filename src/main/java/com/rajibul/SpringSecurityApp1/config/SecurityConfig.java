85d8629075cccd66a7b3dc9229b4ec530042c793
Rajibul <rajibulmolla5@gmail.com>
2024-06-27 01:59:05 +0530

    InMemeory Role Based Authentication

---
+++ src/main/java/com/rajibul/SpringSecurityApp1/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.rajibul.SpringSecurityApp1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+//To config Authentication and othorisetion
+@EnableMethodSecurity
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //Authentication
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails admin= User.withUsername("Rajib")
+                .password(encoder.encode("12345"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user= User.withUsername("John")
+                .password(encoder.encode("1234"))
+                .roles("USER")
+                .build();
+
+        //Create two user and save these inside inmemory.
+        return new InMemoryUserDetailsManager(admin,user);
+    }
+
+    //to use normal password to hash password
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    //Authorization
+    @Bean
+//    @SuppressWarnings("removal")
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+//**************************New Version Of SpringSecurity*******************
+        httpSecurity
+                .csrf(csrf -> csrf.disable()) // Updated way to disable CSRF
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/products").permitAll()
+                        .requestMatchers("/products/{id}").authenticated()
+//                                .requestMatchers("/products/{id}").hasRole("ADMIN")
+                ).formLogin(withDefaults());
+
+        return httpSecurity.build();
+
+//        *********************Old Version of SpringSecurity*****************
+//        return httpSecurity.csrf().disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/products").permitAll()
+//                .and()
+//                .authorizeHttpRequests().requestMatchers("/products/**")
+//                .authenticated().and().formLogin().and().build();
+//        return null;
+    }
+}
--- src/main/java/com/rajibul/SpringSecurityApp1/controller/AdminController.java
+++ src/main/java/com/rajibul/SpringSecurityApp1/controller/AdminController.java
@@ -5,6 +5,7 @@ import com.rajibul.SpringSecurityApp1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,23 +17,28 @@ public class AdminController {
     private ProductService  productService;
 
     @PostMapping("/add")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Product> addProduct(@RequestBody Product newProdect){
         return new ResponseEntity<>(productService.newProduct(newProdect), HttpStatus.CREATED);
     }
     @PutMapping("/update")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Product> updateProduct(@RequestBody Product newProdect){
         return new ResponseEntity<>(productService.updateProductDetails(newProdect),HttpStatus.CREATED);
     }
 
     @DeleteMapping ("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<String> deleteProduct(@PathVariable Integer id){
         return new ResponseEntity<>(productService.deleteProduct(id),HttpStatus.CREATED);
     }
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Product> getProduct(@PathVariable Integer id){
         return new ResponseEntity<>(productService.findProduct(id), HttpStatus.CREATED);
     }
     @GetMapping("")
+//    @PreAuthorize("hasAuthority('ROLE_USER')")
     public ResponseEntity<List<Product>> getAllProduct(){
         return new ResponseEntity<>(productService.getAllProduct(), HttpStatus.CREATED);
     }
